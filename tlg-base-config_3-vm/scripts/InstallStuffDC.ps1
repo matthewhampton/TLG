e3f66b294d2dea1cf71be184b39b0fc696f7085b
Matt Hampton <matt.hampton@hexagon.com>
2019-11-07 09:13:09 +0200

    I'm keeping the pre-installed set to a little bit shorter so it is more likely to work

--- tlg-base-config_3-vm/scripts/InstallStuffDC.ps1
+++ tlg-base-config_3-vm/scripts/InstallStuffDC.ps1
@@ -2,8 +2,5 @@ iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.p
 choco install -y WindowsAzurePowershell
 choco install -y sysinternals
 choco install -y git
-choco install -y googlechrome
 choco install -y 7zip.install
 choco install -y notepadplusplus.install
-choco install -y sql-server-2017
-choco install -y sql-server-management-studio
