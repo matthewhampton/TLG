72bbd9aff312f8c2d77b6cb0c718a46f07dddec9
Matt Hampton <matt.hampton@hexagon.com>
2019-11-07 17:01:47 +0200

    We actually need to do the NetFx3 as well..

--- tlg-base-config_3-vm/scripts/InstallStuffApp.ps1
+++ tlg-base-config_3-vm/scripts/InstallStuffApp.ps1
@@ -1,1 +1,7 @@
-cmd /c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3ServerFeatures
+if(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
+    cmd /c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3ServerFeatures
+    cmd /c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3
+}
+else {
+     Write-Host "Microsoft .Net 3.5 Framework is already installed on your machine."
+}
