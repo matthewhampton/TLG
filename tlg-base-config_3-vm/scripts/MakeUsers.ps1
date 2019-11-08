e61669cbecb62393e30e57ae8b2d536180c4f5d2
Matt Hampton <matt.hampton@hexagon.com>
2019-11-08 13:09:24 +0200

    Also set up for kerberos

--- tlg-base-config_3-vm/scripts/MakeUsers.ps1
+++ tlg-base-config_3-vm/scripts/MakeUsers.ps1
@@ -1,3 +1,6 @@
 
 Import-Module ActiveDirectory
 New-ADUser -Name "j5 Service Account" -SamAccountName "j5-application-user" -UserPrincipalName "j5-application-user@example.com" -AccountPassword(Read-Host -AsSecureString "Input Password") -Enabled $true
+
+setspn -A HTTP/app1 example.com\j5-application-user
+setspn -A HTTP/app1.example.com example.com\j5-application-user
