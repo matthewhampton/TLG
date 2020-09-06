3447c6c6b09f570be1d17b2993e2051651c333f6
Matthew Hampton <matthew.j.hampton@gmail.com>
2020-09-06 13:16:41 +0200

    Need to enable tls1.2 for choco to install now
    See https://chocolatey.org/blog/remove-support-for-old-tls-versions

--- tlg-base-config_3-vm/scripts/InstallStuffDC.ps1
+++ tlg-base-config_3-vm/scripts/InstallStuffDC.ps1
@@ -1,3 +1,5 @@
+get-windowsfeature Net-Framework-45-Core
+[System.Net.ServicePointManager]::SecurityProtocol.HasFlag([Net.SecurityProtocolType]::Tls12)
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y WindowsAzurePowershell
 choco install -y sysinternals
