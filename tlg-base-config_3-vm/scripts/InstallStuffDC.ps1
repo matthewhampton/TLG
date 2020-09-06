9810f739047987218e22782421d65bea4d5db49b
Matthew Hampton <matthew.j.hampton@gmail.com>
2020-09-06 13:21:29 +0200

    The choco package changed name

--- tlg-base-config_3-vm/scripts/InstallStuffDC.ps1
+++ tlg-base-config_3-vm/scripts/InstallStuffDC.ps1
@@ -1,7 +1,7 @@
 Get-WindowsFeature Net-Framework-45-Core
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install -y WindowsAzurePowershell
+choco install -y AzurePowerShell
 choco install -y sysinternals
 choco install -y git
 choco install -y 7zip.install
