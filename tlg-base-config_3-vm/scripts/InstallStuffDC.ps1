bb8381e7ef625c84c9e320dac72bbf67951107ec
Matthew Hampton <matthew.j.hampton@gmail.com>
2020-09-06 13:18:05 +0200

    I prefer that case to read

--- tlg-base-config_3-vm/scripts/InstallStuffDC.ps1
+++ tlg-base-config_3-vm/scripts/InstallStuffDC.ps1
@@ -1,4 +1,4 @@
-get-windowsfeature Net-Framework-45-Core
+Get-WindowsFeature Net-Framework-45-Core
 [System.Net.ServicePointManager]::SecurityProtocol.HasFlag([Net.SecurityProtocolType]::Tls12)
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y WindowsAzurePowershell
